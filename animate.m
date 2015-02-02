baf1f5d9994ecdd88339d924964c1ace9b0b6128
Ho Ching Justin Cheng <hochingc@usc.edu>
2015-02-01 19:35:55 -0800

    first commit animate.m

---
+++ animate.m
@@ -0,0 +1,31 @@
+%takes in group file and animation data file and output animation
+function animate(datafile)
+%read groups into a matrix
+data = fopen(datafile);
+groupfile = fscanf(data,'%s\n',1);
+Groups = dlmread(groupfile);
+[G,N] = size(Groups);
+%positions are stored in Map
+Map = zeros(2,N);
+figure;
+while ~(feof(data))
+    %read an instance of Map from data file
+    for i = 1:N
+        Map(1,i) = fscanf(data,'%f ',1);
+        Map(2,i) = fscanf(data,'%f\n',1);
+    end;
+    %plot an instance of map
+    clf;
+    hold on;
+    axis([0, 20, 0, 20]);
+    for i = 1:N
+        plot(Map(1,i),Map(2,i),'b.');
+    end;
+    for i = 1:G
+        
+    end;
+    drawnow;
+    disp(Map);
+    %pause(10);
+end;
+fclose(data);
