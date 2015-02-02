a93f499a49f3a84f3c8b64fe280fc4fcb49110f9
Ho Ching Justin Cheng <hochingc@usc.edu>
2015-02-01 17:54:03 -0800

    added randomized map

--- simulation.m
+++ simulation.m
@@ -1,10 +1,10 @@
-groups = dlmread('groups.txt');
+Groups = dlmread('groups.txt');
 enfile = fopen('environment.txt');
 eta = fscanf(enfile,'eta=%f\n',1);
-Ptn = fscanf(enfile,'Pt/N=%f\n',1);
+gamma = fscanf(enfile,'Pt/N=%f\n',1);
 side = fscanf(enfile,'side=%f',1);
 fclose(enfile);
-eta
-Ptn
-side
-
+[G,N] = size(Groups);
+Map = side*rand(N,2);
+Groups
+Map
