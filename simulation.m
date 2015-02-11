11f8609802c6a8e25555d1fcd11d17918cee7234
Sang Woon (Ben) Lee <sangwoon@usc.edu>
2015-02-10 22:14:35 -0800

    read first two lines

--- simulation.m
+++ simulation.m
@@ -1,5 +1,13 @@
 function simulation(groupfile,enfile,outfile,simulations,passes)
-Groups = dlmread(groupfile);
+
+%get two line of txt file and put into arrays
+numbers = fopen(groupfile);
+numline = fgetl(numbers);
+Host = sscanf(numline, '%u');
+numline = fgetl(numbers);
+Location = sscanf(numline, '%u');
+
+Groups = dlmread(numbers);
 efile = fopen(enfile);
 eta = fscanf(efile,'eta=%f\n');
 gamma = fscanf(efile,'Pt/N=%f\n');
@@ -7,7 +15,7 @@ side = fscanf(efile,'side=%f');
 fclose(efile);
 [G,N] = size(Groups);
 Map = side*rand(N,2);
-dlmwrite(outfile,groupfile,'');
+dlmwrite(outfile,numbers,'');
 dlmwrite(outfile,enfile,'-append','delimiter','');
 s = str2double(simulations);
 p = str2double(passes);
