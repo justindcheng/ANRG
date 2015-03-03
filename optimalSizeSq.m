64ae8ee23434c777dfa0fee85dcaf65ed63fe308
Sang Woon Lee <sangwoon@usc.edu>
2015-03-02 18:31:09 -0800

    Changed functions

--- optimalSizeSq.m
+++ optimalSizeSq.m
@@ -1,11 +1,13 @@
 function optimalSizeSq(l, u)
     syms s
-    f(s) = (l * (1/((1 + s)^u) ) )/( l * (1/((1 + s *( 2^(1/2)))^u) ) + 1  );
+    f(s) = log((l * (1/((1 + s)^u) ) )/( l * (  (1/((1 + s *( 2^(1/2)))^u)) + (1/((1 + s)^u)) ) + 1  ) + 1) * 8;
     df = diff(f);
     eqn = df == 0;
     solx = solve(eqn, s);
-    disp(double(solx));
-    
+    solx = double(solx);
+    disp(solx);
+    num = solx.s;
+    disp(num);
     
 
 end
