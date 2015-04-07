610eb786ce415617ceb35f7a090301d8decdac21
Sang Woon Lee <sangwoon@usc.edu>
2015-04-06 17:43:42 -0700

    Metric.cpp done

---
+++ c_sim/metric.cpp
@@ -0,0 +1,30 @@
+#include "metric.h"
+#include "util.h"
+#include "robot.h"
+#include <cmath>
+
+Metric::Metric(vector<Robot> M, vector<Robot> G, double eta, double gamma ){
+	int N = (int)G.size();
+	int R = (int)(G[0].size());
+
+	double sum = 0; 
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j< R; j++){
+			for (int k = 0; k < R; k++){
+				if (G[i][j] != 0 && G[i][k] != 0 && j != k  ){
+					double s = Power(M[j] , M[k]  , eta);
+					vector<double> x; 
+					for (int l = 0; l < R; l++ ){
+						x.push_back(0);
+					}
+					for (int m = 0; m < R; m++){
+						if (j != m && k != m){
+							x[m] = Power(M[j], M[m], eta );
+						}
+					}
+					sum = sum + log(1 + SINR(s, x, gamma) );
+				}
+			}
+		}
+	}
+};
--- c_sim/metric.h
+++ c_sim/metric.h
@@ -1,10 +1,17 @@
 #ifndef METRIC_H
 #define METRIC_H
+#include <vector> 
+
+//M is the 2 column matrix 
+//G column robots rows row 
 
 class Metric {
 	public:
-		Metric();
+		Metric(vector<int> M, vector<int> G, double eta, eta gamma );
 		~Metric();
+
+	private:
+		
 
 };
 
