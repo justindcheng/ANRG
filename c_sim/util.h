26a1e7f371f2a710bac4724e65a4dec7f3def763
Ho Ching Justin Cheng <hochingc@usc.edu>
2015-04-06 18:07:58 -0700

    fixed util.h

--- c_sim/util.h
+++ c_sim/util.h
@@ -2,10 +2,11 @@
 #define UTIL_H
 #include "robot.h"
 #include <vector>
-using namespace std;
 
 double Distance(Robot r1, Robot r2);
 
 double Power(Robot r1, Robot r2, double eta);
+
+double SINR(double signal, std::vector<double> interf, double gamma);
 
 #endif
