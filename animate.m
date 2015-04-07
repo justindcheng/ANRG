23904c96b701fd9512f7cfa6752896c997eaf5bc
Ho Ching Justin Cheng <hochingc@usc.edu>
2015-04-06 18:01:36 -0700

    fixed animate

--- animate.m
+++ animate.m
@@ -11,7 +11,7 @@ gamma = fscanf(efile,'Pt/N=%f\n');
 side = fscanf(efile,'side=%f');
 [G,N] = size(Groups);
 %positions are stored in Map
-Map = zeros(2,N);
+Map = zeros(N,2);
 Colors = rand(3,G);
 Colors = ones(3,G) - Colors.*Colors;
 figure;
@@ -20,16 +20,17 @@ time = 0;
 while ~(feof(data))
     %read an instance of Map from data file
     for i = 1:N
-        Map(1,i) = fscanf(data,'%f ',1);
-        Map(2,i) = fscanf(data,'%f\n',1);
+        Map(i,1) = fscanf(data,'%f ',1);
+        Map(i,2) = fscanf(data,'%f\n',1);
     end;
     %plot an instance of map
+    %subplot(2,1,1);
     figure(1);
     clf;
     hold on;
     axis([0,side,0,side]);
     for i = 1:N
-        plot(Map(1,i),Map(2,i),'b.', 'MarkerSize', 30);
+        plot(Map(i,1),Map(i,2),'b.', 'MarkerSize', 30);
     end;
     for i = 1:G
         for j = 1:N
@@ -37,7 +38,7 @@ while ~(feof(data))
                 if (Groups(i,j) == 1) && (Groups(i,k) == 1) && (i ~= j)
                     %d = Distance(Map(1,j),Map(2,j),Map(1,k),Map(2,k))/side/sqrt(2);
                     %d = sqrt(d);
-                    line('XData', [Map(1,j) Map(1,k)], 'YData', [Map(2,j) Map(2,k)], 'LineStyle', '-', ...
+                    line('XData', [Map(j,1) Map(k,1)], 'YData', [Map(j,2) Map(k,2)], 'LineStyle', '-', ...
     'LineWidth', 2, 'Color',[Colors(1,i),Colors(2,i),Colors(3,i)]);
                 end;
             end;
@@ -46,10 +47,11 @@ while ~(feof(data))
     drawnow;
     %plot metric
     figure(2);
+    %subplot(2,1,2);
     hold on;
-    plot(time,Metric(Map,G,eta,gamma),'b.', 'MarkerSize', 5);
+    plot(time,Metric(Map,Groups,eta,gamma),'b.', 'MarkerSize', 5);
     time = time + 1;
-    disp(Metric(Map,G,eta,gamma));
+    %disp(Metric(Map,Groups,eta,gamma));
     %disp(Map);
     %pause(0.05);
 end;
